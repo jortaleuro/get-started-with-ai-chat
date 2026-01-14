82be23495f80a43f2ead2ae51c85e13ba743a2a5
jortaleuro <jortal@eurostar.es>
2026-01-14 16:51:04 +0100

    Update main.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft. All rights reserved.
+#     Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license.
 # See LICENSE file in the project root for full license information.
 import contextlib
@@ -145,3 +145,4 @@ def create_app():
     app.include_router(routes.router)
 
     return app
+
